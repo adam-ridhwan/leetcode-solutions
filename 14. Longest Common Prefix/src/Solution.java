fc833bb65f2aa45b14c30e522ab3881c438a462c
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-06-22 12:43:16 -0400

    added problem 14

---
+++ 14. Longest Common Prefix/src/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+        }
+        System.out.println(prefix);
+    }
+}
